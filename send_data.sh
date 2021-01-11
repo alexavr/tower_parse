3a2094a23a1590b0dc795fda81495b067f53b0cc
Dmitry Vukolov <dmvuko@gmail.com>
2021-01-11 14:58:46 +0300

    Adapt rsync to the new .npz saving scheme

--- send_data.sh
+++ send_data.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # crontab -e -> /home/gavr/TowerMSU/send_data.sh
-for i in $(ls /home/gavr/TowerMSU/data/*.npz); do
-	rsync -a ${i} naad-tower:/public/TOWER/npz/
-	if [ "$?" -eq "0" ]; then
-  		rm -rf ${i}
-	fi
-done
+rsync -a --exclude='*.tmp' --remove-source-files /home/gavr/TowerMSU/data/ naad-tower:/public/TOWER/npz/
