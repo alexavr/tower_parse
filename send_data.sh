a3c2f318ca2610abd8e28219e6b4cb633aa4cd84
Dmitry Vukolov <dmvuko@gmail.com>
2021-01-11 16:25:03 +0300

    Updated the remote path

--- send_data.sh
+++ send_data.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # crontab -e -> /home/gavr/TowerMSU/send_data.sh
-rsync -a --exclude='*.tmp' --remove-source-files /home/gavr/TowerMSU/data/ naad-tower:/public/TOWER/npz/
+
+SOURCE=/home/gavr/TowerMSU/data/
+DEST=naad-tower:/var/www/data/domains/tower.ocean.ru/html/flask/data/npz/
+
+rsync -a --exclude='*.tmp' --remove-source-files "$SOURCE" "$DEST"
