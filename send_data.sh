eb3f3096da9b24785dda5b380cb6ad556e69c6f5
alexavr <gavr@sail.msk.ru>
2023-07-07 15:33:44 +0000

    Major update (KLD tower)

--- send_data.sh
+++ send_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # crontab -e -> /home/gavr/TowerMSU/send_data.sh
 
-SOURCE=/home/gavr/TowerMSU/data/
-DEST=naad-tower:/var/www/data/domains/tower.ocean.ru/html/flask/data/npz/
+SOURCE=/home/tower/tower_parse/data/
+DEST=data-tower:/var/www/data/domains/tower.ocean.ru/html/flask/data/npz/
 
 (
     flock -n 9 || exit 1
